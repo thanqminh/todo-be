d8110b026fef3829f44837d5473480a61cbac446
Minh Than <mthan@openwt.com>
2017-07-07 10:07:55 +0700

    fix name

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     put 'share' => 'share_tasks#update'
     delete 'share' => 'share_tasks#delete'
   end
-  get 'shared' => 'task_list#shared'
+  get 'shared' => 'task_lists#shared'
   get 'search/:name' => 'todos#search'
   get 'users' => 'users#index'
   mount_devise_token_auth_for 'User', at: 'auth'
