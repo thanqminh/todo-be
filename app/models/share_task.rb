380b171a21a698ed8d743bf1b7cb13aefcdd206e
Minh Than <mthan@openwt.com>
2017-07-07 11:51:41 +0700

    prevent share task to owner

--- app/models/share_task.rb
+++ app/models/share_task.rb
@@ -2,4 +2,10 @@ class ShareTask < ApplicationRecord
   belongs_to :user
   belongs_to :task_list
   validates :user_id, :uniqueness => {:scope => [:task_list_id]}
+
+  validate :owner_check
+
+  def owner_check
+    errors.add(:user_id, "Can't share to task owner.") if (self.task_list_id.present? && self.task_list.user_id == self.user_id)
+  end
 end
