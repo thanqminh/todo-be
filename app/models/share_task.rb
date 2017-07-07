f8a422d9a64075cf89b9543cd36e9cadf88968cf
Minh Than <mthan@openwt.com>
2017-07-07 10:31:31 +0700

    fix share

--- app/models/share_task.rb
+++ app/models/share_task.rb
@@ -1,5 +1,5 @@
 class ShareTask < ApplicationRecord
   belongs_to :user
   belongs_to :task_list
-  validates :id, :uniqueness => {:scope => [:user_id, :task_list_id]}
+  validates :user_id, :uniqueness => {:scope => [:task_list_id]}
 end
