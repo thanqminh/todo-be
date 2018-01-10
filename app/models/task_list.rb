812bd40da4776c7813330504600b4e638d327f7e
Minh Than <thanqminh@gmail.com>
2018-01-10 15:05:24 +0700

    fix models

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,4 +1,4 @@
-class TaskList < AnotherApplicationRecord
+class TaskList < ApplicationRecord
   belongs_to :user
   has_many :todos, dependent: :destroy
   has_many :share_tasks, dependent: :destroy
